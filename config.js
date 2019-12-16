e9968b0ede3a1e307713c85b94ef7d842ce393cc
ogheneovo12 <49083654+ogheneovo12@users.noreply.github.com>
2019-12-16 18:31:59 +0100

    deleted config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  db:"mongodb://mlhhack:XXLadPABnsV00qBjscKbjpODGHidwvM7owXi4wJOiRsYXGu1m6Y7218yqsBgwpNXun19n7lfwEw6SE7tliLUJA==@mlhhack.documents.azure.com:10255/admin?ssl=true&replicaSet=globaldb",
-  
-}
